0a0e87ff85dd40ca3a86a255336e4407dee7ec4c
Suhaas-A <155752982+Suhaas-A@users.noreply.github.com>
2025-11-02 19:45:57 +0530

    Update database.py

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,6 +1,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-from backend.core.config import settings
+from core.config import settings
 
 engine = create_engine(settings.DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -15,3 +15,4 @@ def get_db():
 
 def create_tables():
     Base.metadata.create_all(bind=engine, checkfirst=True)
+
